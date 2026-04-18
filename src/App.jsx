0ea35bf09ab1f6a9bdb5e3084696026e3454f938
Rodrigo Costa <ianrodrigo25@gmail.com>
2026-04-18 09:49:07 -0300

    feat: add BrowserRouter and route skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
 import FacturaUpload from './components/FacturaUpload'
+import PlanPage from './pages/PlanPage'
 
 export default function App() {
-  return <FacturaUpload />
+  return (
+    <Routes>
+      <Route path="/" element={<FacturaUpload />} />
+      <Route path="/plan" element={<PlanPage />} />
+    </Routes>
+  )
 }
